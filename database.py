cd3e181050bc49f9c340814d9baaf6a92c35fa70
eww0l <arellanosamir0@gmail.com>
2026-06-05 00:58:02 -0500

    ads

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import os
 from supabase import create_client, Client
 
-# Leeremos las credenciales directamente del servidor en internet
-url: str = os.environ.get("https://svfgjmudruxjijkimwdj.supabase.co")
-key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2ZmdqbXVkcnV4amlqa2ltd2RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MjY1MzksImV4cCI6MjA5NjIwMjUzOX0.MxWcYEOmF0k3dv5Cv0G91_pYebbRq7A-dAmRUR3_lP0")
+# Esto le dice a Python: "Busca las etiquetas que guardamos en Render"
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 
 supabase: Client = create_client(url, key)
